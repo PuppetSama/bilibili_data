a9a696758ef6ef891f47122ba3499e5ccd5910e6
PuppetSama <puppetk108@gmail.com>
2016-08-02 22:27:39 +0800

    initial version

---
+++ bilibili.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+import urllib
+from urllib.request import *
+
+
+page = 1
+url = 'http://www.bilibili.com'
+try:
+    request = Request(url)
+    response = urlopen(request)
+    print(response.read())
+except(urllib2.URLError, e):
+    if hasattr(e,"code"):
+        print(e.code)
+    if hasattr(e,"reason"):
+        print(e.reason)
