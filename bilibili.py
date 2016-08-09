1a80b7a2d6a6e1920611e804ac0bda5a66a85227
PuppetSama <puppetk108@gmail.com>
2016-08-09 22:55:56 +0800

    sad

--- bilibili.py
+++ bilibili.py
@@ -1,15 +1,39 @@
 # -*- coding:utf-8 -*-
-import urllib
-from urllib.request import *
+#import urllib
+#from urllib.request import *
 
 
-url = 'http://www.bilibili.com'
-try:
-    request = Request(url)
-    response = urlopen(request)
-    print(response.read())
-except(urllib2.URLError, e):
-    if hasattr(e,"code"):
-        print(e.code)
-    if hasattr(e,"reason"):
-        print(e.reason)
+#url = 'http://www.bilibili.com'
+#try:
+#    request = Request(url)
+#    response = urlopen(request)
+#    print(response.read())
+#except(urllib2.URLError, e):
+#	if hasattr(e,"code"):
+#        print(e.code)
+#    if hasattr(e,"reason"):
+#        print(e.reason)
+
+import urllib.request
+import gzip
+
+def getUrlContent(url):  
+    #返回页面内容  
+    doc = urllib.request.urlopen(url).read()  
+    #解码  
+    try:  
+        html=gzip.decompress(doc).decode("utf-8")  
+    except:  
+        html=doc.decode("utf-8")  
+    return html  
+
+rage = 1
+url = "http://www.bilibili.com/video/douga-mad-1.html"
+#data = urllib.request.urlopen(url).read()
+data =getUrlContent(url)
+#buf = StringIO.StringIO(<response object>.content)
+#gzip_f = gzip.GzipFile(fileobj=buf)
+#content = gzip_f.read()
+
+#data = data.decode('utf-8')
+print(data)
